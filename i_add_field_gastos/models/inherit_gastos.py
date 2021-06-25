5d8fde92d529695485ad3901addaeca62fe882a6
sistemas-insco <59057099+sistemas-insco@users.noreply.github.com>
2021-06-25 16:07:21 -0500

    Add-purchase-order

--- i_add_field_gastos/models/inherit_gastos.py
+++ i_add_field_gastos/models/inherit_gastos.py
@@ -9,3 +9,4 @@ class GastosUsuario(models.Model):
 
 	proveedor_id = fields.Many2one('res.partner', string='Proveedor')
 	fecha_pago_id = fields.Date(string='Fecha de pago', track_visibility="always")
+	purchase_order_id = fields.Many2one('purchase.order', string='Orden de Compra')
