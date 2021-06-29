954fff85129f3a1e31f794fe844cf8ebc48724c5
sistemas-insco <59057099+sistemas-insco@users.noreply.github.com>
2021-06-29 07:43:50 -0500

    vista-gastos11

--- i_add_field_gastos/models/inherit_gastos.py
+++ i_add_field_gastos/models/inherit_gastos.py
@@ -10,6 +10,7 @@ class GastosUsuario(models.Model):
 	proveedor_id = fields.Many2one('res.partner', string='Proveedor')
 	fecha_pago_id = fields.Date(string='Fecha de pago', track_visibility="always")
 	purchase_order_id = fields.Many2one('purchase.order', string='Orden de Compra')
-	sucursal_id = fields.Selection([('Insco de Mexico:', 'Insco de Mexico:'), ('Insco Guadalajara:', 'Insco Guadalajara:'), ('Insco Merida:', 'Insco Merida:'), ('Insco Bajio:', 'Insco Bajios:')],
+	sucursal_id = fields.Selection([('Insco de Mexico:', 'Insco de Mexico:'), ('Insco Guadalajara:', 'Insco Guadalajara:'), ('Insco Merida:', 'Insco Merida:'), ('Insco Bajio:', 'Insco Bajio:')],
         string='Sucursal')
 	diferencia_id = fields.Monetary(string='Diferencia', track_visibility="always")
+	departamento_id = fields.Many2one(string='Departamento', related='employee_id.department_id')
