d41f9022225e6d18d14f73c95523634e48958aa1
sistemas-insco <59057099+sistemas-insco@users.noreply.github.com>
2021-08-02 13:42:51 -0500

    Menu Vacaciones02

--- i_add_field_empleados_vacaciones/models/add_fields_solicitud_vacaciones.py
+++ i_add_field_empleados_vacaciones/models/add_fields_solicitud_vacaciones.py
@@ -13,7 +13,7 @@ class vacaciones(models.Model):
 	# 	return self.env['res.users'].search([('name', '=', self.env.uid)], limit=1)
 	
 	#empleado_id = fields.Many2one('hr.employee',track_visibility="always", string='Empleado', default=_default_empleado_id)
-	name = fields.Char('Periódo',default='Periódo 2021', required=True)
+	name = fields.Char('Periódo',default='Periódo 2021', track_visibility="always",required=True)
 	empleado_id = fields.Many2one('hr.employee',track_visibility="always", string="Empleado", required=True)
 	usuario_responsable=fields.Many2one("hr.employee", string="Responsable", required=True)
 	user_id = fields.Many2one('res.users', string='Usuario', required=True)
@@ -22,30 +22,36 @@ class vacaciones(models.Model):
 	departamento_id = fields.Many2one('hr.department', string="Departamento")
 	numero_dias_solicitaso =fields.Integer(string="Días solicitados",  track_visibility="always", required=True)
 	numero_dias_restantes = fields.Integer(string="Días restantes", track_visibility="always")
-	dias_vacaciones = fields.Integer(string="Días de Vacaciones", related = 'empleado_id.vacaciones_dias', store = True)
+	dias_vacaciones = fields.Integer(string="Días de Vacaciones", related = 'empleado_id.vacaciones_dias')
 	
 	state = fields.Selection([
 										('nuevo', 'Nuevo'),
 										('en proceso', 'En Proceso'),
 										('aprobado', 'Aprobado'),
 										('cancelado', 'Cancelado')],
-										default='nuevo', string="Estado",track_visibility="always")
+										default='nuevo', string="Estado",track_visibility="always",copy=False)
 
 	solicitud_tipo =fields.Selection([('vacaciones', 'Vacaciones'), ('incapacidad', 'Incapacidad'),('permiso', 'Permiso'), ('reposicion', 'Reposición'),('otro', 'Otro')], string="Tipo de Solicitud", track_visibility="always")
 
 
+
 	@api.multi
-	def solictud_aprovada(self):
-		self.write({'state': 'aprobado'})
+	def solictud_aprovada(self, reason):
+		if self.user_has_groups('i_add_field_empleados_vacaciones.group_vacaiones_manager'):
+				self.write({'state': 'aprobado'})
+
+	@api.multi
+	def solictud_cancelada(self, reason):
+		if self.user_has_groups('i_add_field_empleados_vacaciones.group_vacaiones_manager'):
+				self.write({'state': 'cancelado'})
+
+	@api.multi
+	def solictud_enproceso(self, reason):
+				self.write({'state': 'en proceso'})
+		
 
 
 	@api.onchange('numero_dias_solicitaso','solicitud_tipo')
 	def dia_vacaiones_restantes(self):
 		if self.solicitud_tipo == 'vacaciones':
 			self.numero_dias_restantes = self.dias_vacaciones - self.numero_dias_solicitaso
-
-
-	# @api.onchange('típo_solicitud')
-	# def tipo_vacaciones(self):
-	# 	if típo_solicitud == 'vacaciones'
-	# 		dias_vacaciones = dias_vacaciones = fields.Integer(related = 'empleado_id.vacaciones_dias')
